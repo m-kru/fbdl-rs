1a599862c1fe2ca34162c18a2f0b218a5851ec97
Michał Kruszewski <mkru@protonmail.com>
2024-03-27 21:11:04 +0100

    args: Use enum for parameters instead of str

--- src/args.rs
+++ src/args.rs
@@ -1,40 +1,56 @@
+use std::fmt;
 use std::path::PathBuf;
 
 #[derive(Debug)]
 pub struct Args {
     main: String,
     consts_dump_path: Option<PathBuf>,
+    reg_results_path: Option<PathBuf>,
 }
 
-enum State<'a> {
+enum Param {
+    Main,
+    RegResultsPath,
+    ConstsDumpPath,
+}
+
+impl fmt::Display for Param {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Param::Main => write!(f, "-main"),
+            Param::ConstsDumpPath => write!(f, "-c"),
+            Param::RegResultsPath => write!(f, "-r"),
+        }
+    }
+}
+
+enum State {
     FlagOrParam,
-    Arg(&'a str),
-    MaybeArg(&'a str),
+    Arg(Param),
+    MaybeArg(Param),
 }
 
 fn is_valid_param(s: &str) -> bool {
     match s {
-        "-main" | "-c" => true,
-        _ => false
+        "-main" | "-c" | "-r" => true,
+        _ => false,
     }
 }
 
-impl<'a> State<'a> {
+impl State {
     fn handle_param(&mut self, param: String) {
         match self {
-            State::FlagOrParam | State::MaybeArg(_) => {
-                match param.as_str() {
-                    "-main" =>  {
-                        *self = State::Arg("-main")
-                    }
-                    "-c" =>  {
-                        *self = State::MaybeArg("-c")
-                    }
-                    _ => todo!(),
-                }
-            }
+            State::FlagOrParam | State::MaybeArg(_) => match param.as_str() {
+                "-main" => *self = State::Arg(Param::Main),
+                "-c" => *self = State::MaybeArg(Param::ConstsDumpPath),
+                "-r" => *self = State::MaybeArg(Param::RegResultsPath),
+                _ => todo!(),
+            },
             State::Arg(p) => {
-                eprintln!("expecting argument for parameter '{}', found parameter '{}'", p, param);
+                eprintln!(
+                    "expected argument for parameter '{}', found parameter '{}'",
+                    p, param
+                );
                 std::process::exit(1);
             }
         }
@@ -42,33 +58,39 @@ impl<'a> State<'a> {
 
     fn handle_arg(&mut self, arg: String, args: &mut Args) {
         match self {
-            State::Arg(param) => {
-                match param {
-                    &mut "-main" => args.main = arg,
-                    _ => todo!(),
-                }
+            State::FlagOrParam => {
+                eprintln!("expected flag or paramter, found '{}'", arg);
+                std::process::exit(1);
             }
-            State::MaybeArg(param) => {
-                match param {
-                    &mut "-c" => args.consts_dump_path = Some(PathBuf::from(arg)),
-                    _ => todo!(),
-                }
-            }
-            _ => todo!(),
+            State::Arg(param) => match param {
+                Param::Main => args.main = arg,
+                _ => todo!(),
+            },
+            State::MaybeArg(param) => match param {
+                Param::Main => panic!("should never happen"),
+                Param::ConstsDumpPath => args.consts_dump_path = Some(PathBuf::from(arg)),
+                Param::RegResultsPath => args.reg_results_path = Some(PathBuf::from(arg)),
+            },
         }
     }
-
 }
 
 pub fn parse() -> Args {
-    let mut args = Args{main: "main".to_string(), consts_dump_path: None};
+    let mut args = Args {
+        main: "main".to_string(),
+        consts_dump_path: None,
+        reg_results_path: None,
+    };
     let mut state = State::FlagOrParam;
 
     let args_count = std::env::args().len() - 2;
     for (i, arg) in std::env::args().skip(1).enumerate() {
         if i == args_count {
             if let State::Arg(param) = state {
-                eprintln!("missing path to .fbd file or argument for parameter '{}'", param);
+                eprintln!(
+                    "missing path to .fbd file or argument for parameter '{}'",
+                    param
+                );
                 std::process::exit(1);
             }
         }
